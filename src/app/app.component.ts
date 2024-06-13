4b3054379ad9cb605dba96d50b29c47e9811e802
alexhetley6107 <hetleyofficial@gmail.com>
2024-06-13 16:28:09 +0300

    feat: layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -7,11 +7,21 @@ import { ProfileHeaderComponent } from '../../widgets/profile-header/profile-hea
 import { AsyncPipe } from '@angular/common';
 import { SvgIconComponent } from '../../widgets/svg-icon/svg-icon.component';
 import { ImgUrlPipe } from '../../shared/pipes/img-url.pipe';
+import { PostComponent } from './post/post.component';
+import { PostFeedComponent } from './post-feed/post-feed.component';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [ProfileHeaderComponent, AsyncPipe, SvgIconComponent, RouterLink, ImgUrlPipe],
+  imports: [
+    ProfileHeaderComponent,
+    AsyncPipe,
+    SvgIconComponent,
+    RouterLink,
+    ImgUrlPipe,
+    PostComponent,
+    PostFeedComponent,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
